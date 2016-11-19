e92280454d0f641555ea6744d09734d956956896
Alex Wright <alex@xeentech.com>
2016-11-19 09:59:24 +0000

    Adding Promise based ready() API to Collection

--- mybone.js
+++ mybone.js
@@ -2,4 +2,23 @@ var Backbone = require('./xeenbone');
 
 Backbone.ajax = require('backbone.fetch');
 
+Backbone.Collection = Backbone.Collection.extend({
+    // init _ready state is false
+    _ready: false,
+    ready: function () {
+        return new Promise((resolve, reject) => {
+            if (this._ready) {
+                resolve(this);
+            }
+            else {
+                this.listenTo(this, 'sync', () => {
+                    this._ready = true;
+                    resolve();
+                });
+                this.fetch();
+            }
+        });
+    },
+});
+
 module.exports = Backbone;
