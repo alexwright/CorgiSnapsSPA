eb7ab9766951d003c71d32bd0f9b7508ffd76fa9
Alex Wright <alex@xeentech.com>
2016-11-19 09:36:59 +0000

    Cut down version of Backbone without query or View

--- views/snap_list.js
+++ views/snap_list.js
@@ -1,37 +1,10 @@
-var BaseView = require('./baseview'),
+var React = require('react'),
     Snaps = require('../collections/snaps'),
-    SnapThumbView = require('./snap_thumb.js'),
     SnapListComponent = require('../components/snap_list.jsx')
 
-var SnapListView = BaseView.extend({
-        componentClass: SnapListComponent,
-        getSnapThumbs: function () {
-            return this.collection.map(function (model) {
-                return new SnapThumbView({
-                    model: model,
-                });
-            }, this);
-        },
-        getViewData: function () {
-            console.log('SnapListView.getViewData(): ', this.getSnapThumbs());
-            return {
-                snaps: this.getSnapThumbs(),
-            };
-        },
-    },
-    {
-        fromRoute: function (route, urlArgs) {
-            if (typeof window !== 'undefined') {
-                Snaps.fetch();
-            }
-            else {
-                var data = [{"id":1,"image":"http://corgisnaps.com/media/IMG_9455_uXh7kG4.jpg"}];
-            }
-            var view = new SnapListView({
-                collection: Snaps,
-            });
-            return view;
-        },
+var SnapListView = function (route, urlArgs) {
+    return React.createElement(SnapListComponent, {
+        collection: Snaps,
     });
-
+}
 module.exports = SnapListView;
